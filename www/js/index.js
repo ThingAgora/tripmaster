5e0564a0b67dc9d8c984d138701eb99459df3b36
Luc Yriarte <luc.yriarte@intel.com>
2013-09-03 11:49:30 +0200

    iinitial commit

---
+++ www/js/index.js
@@ -0,0 +1,21 @@
+/*
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+		this.statusArea = document.getElementById('statusArea');
+		this.statusArea.innerHTML = "initializing...";
+    },
+
+    // Bind Event Listeners
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    // deviceready Event Handler - The scope of 'this' is the event.
+    onDeviceReady: function() {
+		app.statusArea.innerHTML = "ready";
+    }
+};
