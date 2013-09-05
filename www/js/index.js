1297009c2c102ca948e3838d0a271f11eb30f7ba
Luc Yriarte <luc.yriarte@intel.com>
2013-09-05 17:38:57 +0200

    speedometer using geolocation API

--- www/js/index.js
+++ www/js/index.js
@@ -13,8 +13,34 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
 
+    // Error management
+    errorCb: function() {
+    	app.speedometer.innerHTML = "XXX";
+    },
+    
+    // Geolocation management
+    positionWatchId: -1,
+    positionWatchCb: function(position) {
+    	if (position.coords.speed) {
+    		var kmh = position.coords.speed * 3.6;
+    		app.speedometer.innerHTML = kmh.toFixed(0);
+    	}
+    	else
+    		app.speedometer.innerHTML = "000";
+    },
+    positionWatchOptions: {
+    	frequency: 1000,
+    	maximumAge: 60000, 
+    	timeout: 10000,
+    	enableHighAccuracy: true
+    },
+    
     // deviceready Event Handler - The scope of 'this' is the event.
     onDeviceReady: function() {
-		app.speedometer.innerHTML = "000";
+		app.speedometer.innerHTML = "---";
+		if (navigator.geolocation)
+			app.positionWatchId = navigator.geolocation.watchPosition(app.positionWatchCb, app.errorCb, app.positionWatchOptions);
+		else
+			errorCb();
     }
 };
